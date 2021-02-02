d299643887574eed34c2a87b739b277b993ae3d0
Andy <andy27479@gmail.com>
2021-02-02 22:32:12 +0800

    adjusted service worker promise

--- assets/scripts/writer.js
+++ assets/scripts/writer.js
@@ -36,10 +36,16 @@ $("#save-post").click(() => {
 // })
 
 window.onload = () => {
-    'use strict';
-
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('./sw.js');
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/sw.js')
+                .then(function (registration) {
+                    // Registration was successful
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, function (err) {
+                    // registration failed :(
+                    console.log('ServiceWorker registration failed: ', err);
+                });
+        });
     }
 }
