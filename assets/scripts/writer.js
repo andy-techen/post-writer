a250c71a13ec2a39079ad07eeb3dd28242d064b7
Andy <andy27479@gmail.com>
2021-02-02 23:42:20 +0800

    fixing service worker scope for gh-pages

--- assets/scripts/writer.js
+++ assets/scripts/writer.js
@@ -37,7 +37,7 @@ $("#save-post").click(() => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('/sw.js', { scope: '/' })
             .then((registration) => {
                 // Registration was successful
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
